7f4d8ea73774de3e5d36567b407928802429650a
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2024-06-06 16:15:06 -0400

    Update

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
+let selectedVersion = '';
+
+
+function toggleOptions() {
+    document.querySelector('.custom-options').classList.toggle('open');
+    document.querySelector('.custom-select').classList.toggle('open');
+}
+
+function selectVersion(version) {
+    selectedVersion = version;
+    document.querySelector('.custom-select').textContent = `Selected: ${version}`;
+    toggleOptions();
+}
 
 function playGame() {
-    const selectedVersion = document.getElementById('version-select').value;
-    window.location.href = selectedVersion;
+    if (!selectedVersion) {
+        alert('Please select a version to play.');
+        return;
+    }
+    window.location.href = selectedVersion + '/index.html';
 }
-document.addEventListener('DOMContentLoaded', function() {
-    const repoOwner = 'SpeedSlicer'; // Replace with your GitHub username or organization name
-    const repoName = 'MineXLauncher-Beta'; // Replace with your GitHub repository name
 
-    const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/releases`;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(releases => {
-            const releasesDiv = document.getElementById('github-releases');
-            if (releases.length > 0) {
-                releasesDiv.innerHTML = '<h3>Latest Releases</h3>';
-                const ul = document.createElement('ul');
-                releases.slice(0, 5).forEach(release => {
-                    const li = document.createElement('li');
-                    const releaseLink = document.createElement('a');
-                    releaseLink.href = release.html_url;
-                    releaseLink.textContent = release.name;
-                    li.appendChild(releaseLink);
-                    ul.appendChild(li);
-                });
-                releasesDiv.appendChild(ul);
-            } else {
-                releasesDiv.innerHTML = '<p>No releases found.</p>';
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching GitHub releases:', error);
-            const releasesDiv = document.getElementById('github-releases');
-            releasesDiv.innerHTML = '<p>Error fetching releases.</p>';
-        });
-});
+function redirectToNews() {
+    window.location.href = 'news.html';
+}
+
+function redirectToSettings() {
+    window.location.href = 'settings.html';
+}
+function redirectToList() {
+    window.location.href = 'serverlist.html';
+}
+function redirectToMain() {
+    window.location.href = 'index.html';
+}
