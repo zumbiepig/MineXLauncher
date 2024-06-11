c7262905472f0f01243db3a303986c75672e4628
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2024-06-11 19:07:56 -0400

    fixes

---
+++ detector.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to get a cookie value by name
+    function getCookie(name) {
+        let cookieArr = document.cookie.split(";");
+        for(let i = 0; i < cookieArr.length; i++) {
+            let cookiePair = cookieArr[i].split("=");
+            if(name === cookiePair[0].trim()) {
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+        return null;
+    }
+
+    // Set the username placeholder from the cookie
+    let username = getCookie("username");
+    if (username != null) {
+        document.getElementById("usernameInput").placeholder = username;
+        document.getElementById("profile-name").textContent = username;
+    }
+    let modMakerKitEnabled = getCookie("modMakerKitEnabled");
+    if (modMakerKitEnabled === "true") {
+        document.getElementById("modMakerCheckbox").checked = true;
+        document.getElementById("modMakerItem").style.display = "flex";
+        document.getElementById("apiItem").style.display = "flex";
+    } else {
+        document.getElementById("modMakerItem").style.display = "none";
+        document.getElementById("apiItem").style.display = "none";
+    }
+    let serverToolEnabled = getCookie("serverToolEnabled");
+    if (modMakerKitEnabled === "true") {
+        document.getElementById("serverToolCheckbox").checked = true;
+        document.getElementById("serverTool").style.display = "flex";
+    } else {
+        document.getElementById("serverTool").style.display = "none";
+    }
+    // Set the mod maker checkbox state from the cookie
+    
+});
