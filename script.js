898be28686e19a2c7a60b1a9c79016693efff74a
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-06-12 10:49:15 -0700

    add back createabout

--- script.js
+++ script.js
@@ -20,7 +20,18 @@ function playGame() {
     window.location.href = selectedVersion + '/';
 }
 
-
+function createAbout(url) {
+    var win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.src = url;
+    win.document.body.appendChild(iframe);
+}
 
 function redirectToMain() {
     window.location.href = '/';
@@ -37,7 +48,6 @@ function redirectToList() {
 function redirectToClientDownload() {
     window.location.href = '/clientdownload/';
 }
-
 function openEaglerForgeModMaker() {
     createAbout('https://eaglerforge-builder.vercel.app')
 }
