0a0fc9902c5c702c2ffcebcf1768d05c962def87
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2024-06-13 14:13:15 -0400

    c

--- other/archive/archive.js
+++ other/archive/archive.js
@@ -72,9 +72,115 @@ function openOldB13Client() {
     // Get the selected version from the dropdown
     const versionSelect = document.getElementById('13-client-version');
     const selectedVersion = versionSelect.value;
-
+    
     // Construct the URL based on the selected version
     const url = `https://archive.eaglercraft.rip/Eaglercraft_b1.3/client/${selectedVersion}/index.html`;
+
+    // Open the URL in a new window or tab
+    window.open(url, '_blank');
+    var win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    
+    if (faviconURL) {
+        var favicon = win.document.createElement('link');
+        favicon.rel = 'shortcut icon';
+        favicon.type = 'image/x-icon';
+        favicon.href = faviconURL;
+        win.document.head.appendChild(favicon);
+    }
+    
+    if (title) {
+        win.document.title = title;
+    }
+    
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.src = url;
+    
+    win.document.body.appendChild(iframe);
+}
+function opena126Client() {
+    // Get the selected version from the dropdown
+    const versionSelect = document.getElementById('a126-client-version');
+    const selectedVersion = versionSelect.value;
+   
+    // Construct the URL based on the selected version
+    const url = `https://speedslicer.github.io/MineXLauncher-Archive/a1.2.6/${selectedVersion}.html`;
+
+    // Open the URL in a new window or tab
+    window.open(url, '_blank');
+    var win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    
+    if (faviconURL) {
+        var favicon = win.document.createElement('link');
+        favicon.rel = 'shortcut icon';
+        favicon.type = 'image/x-icon';
+        favicon.href = faviconURL;
+        win.document.head.appendChild(favicon);
+    }
+    
+    if (title) {
+        win.document.title = title;
+    }
+    
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.src = url;
+    
+    win.document.body.appendChild(iframe);
+}
+
+function openb173Client() {
+    // Get the selected version from the dropdown
+    const versionSelect = document.getElementById('b173-client-version');
+    const selectedVersion = versionSelect.value;
+
+    // Construct the URL based on the selected version
+    const url = `https://speedslicer.github.io/MineXLauncher-Archive/b1.7.3/${selectedVersion}.html`;
+
+    // Open the URL in a new window or tab
+    window.open(url, '_blank');
+    var win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    
+    if (faviconURL) {
+        var favicon = win.document.createElement('link');
+        favicon.rel = 'shortcut icon';
+        favicon.type = 'image/x-icon';
+        favicon.href = faviconURL;
+        win.document.head.appendChild(favicon);
+    }
+    
+    if (title) {
+        win.document.title = title;
+    }
+    
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.src = url;
+    
+    win.document.body.appendChild(iframe);
+}
+function openinfClient() {
+    // Get the selected version from the dropdown
+    const versionSelect = document.getElementById('b173-client-version');
+    const selectedVersion = versionSelect.value;
+
+    // Construct the URL based on the selected version
+    const url = `https://speedslicer.github.io/MineXLauncher-Archive/VeryOld/${selectedVersion}.html`;
 
     // Open the URL in a new window or tab
     window.open(url, '_blank');
