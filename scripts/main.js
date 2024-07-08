801c530f6b0ceec34f31309316bb7951bb4d5005
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-07-08 11:07:23 -0700

    .

--- scripts/main.js
+++ scripts/main.js
@@ -113,12 +113,7 @@ function setCookie(name, value, days) {
     expires = "; expires=" + date.toUTCString();
   }
   document.cookie =
-    name +
-    "=" +
-    (value || "") +
-    expires +
-    "; path=/; domain=" +
-    window.location.hostname.replace(/^www\./, "");
+    name + "=" + (value || "") + expires + "; path=/; domain=" + window.location.hostname.replace(/^www\./, "");
 }
 
 function createFullscreenEmbed(url) {
