f5b66e41e98006307eff39680757f29912cbeeef
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2024-07-18 15:41:05 -0400

    yep

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+app.get('/mobile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'mobile', 'index.html'));
+});
+
