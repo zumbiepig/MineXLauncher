5dec31ccbccb2e4a153c12377560237ef5e74ab8
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-07-20 06:30:01 -0700

    fix?

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,5 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+  // ...tseslint.configs.stylistic,
 );
