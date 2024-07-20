11dcb8e773fce99c6b2babd7007c4883bd9bfcd4
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-07-20 07:54:55 -0700

    type checking

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,14 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
 );
