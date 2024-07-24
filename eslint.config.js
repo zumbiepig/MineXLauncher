9c9c8196b39692d7cc7f85ce2431b80418c18b52
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-07-24 13:05:45 -0700

    MineXLauncher 1.1

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,12 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  {
-    ignores: ['node_modules'],
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-);
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked, {
+	ignores: ["node_modules"],
+	languageOptions: {
+		parserOptions: {
+			project: true,
+			tsconfigRootDir: import.meta.dirname,
+		},
+	},
+});
