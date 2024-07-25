2379faafdcf057dc7baa8bcec993e786fc2b547d
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-07-25 15:09:32 -0700

    .

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-import createError from "http-errors";
-import express, { json, urlencoded, static as serveStatic } from "express";
-import { join } from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import debug from "debug";
-import { createServer } from "http";
-import indexRouter from "./routes/index.js";
-import usersRouter from "./routes/users.js";
+import createError from 'http-errors';
+import express, { json, urlencoded, static as serveStatic } from 'express';
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import debug from 'debug';
+import { createServer } from 'http';
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
 
 const __dirname = import.meta.dirname;
 
 const app = express();
 
-app.set("views", join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(serveStatic(join(__dirname, "public")));
+app.use(serveStatic(join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.use(function (req, res, next) {
 	next(createError(404));
@@ -30,22 +30,22 @@ app.use(function (req, res, next) {
 
 app.use(function (err, req, res, next) {
 	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 	res.status(err.status || 500);
-	res.render("error");
+	res.render('error');
 });
 
-const debugLogger = debug("minexlauncher:server");
+const debugLogger = debug('minexlauncher:server');
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 const server = createServer(app);
 
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 function normalizePort(val) {
 	const port = parseInt(val, 10);
@@ -59,17 +59,17 @@ function normalizePort(val) {
 }
 
 function onError(error) {
-	if (error.syscall !== "listen") {
+	if (error.syscall !== 'listen') {
 		throw error;
 	}
-	const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+	const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 	switch (error.code) {
-		case "EACCES":
-			console.error(bind + " requires elevated privileges");
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
 			process.exit(1);
 			break;
-		case "EADDRINUSE":
-			console.error(bind + " is already in use");
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
 			process.exit(1);
 			break;
 		default:
@@ -79,6 +79,6 @@ function onError(error) {
 
 function onListening() {
 	const addr = server.address();
-	const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-	debugLogger("Listening on " + bind);
+	const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+	debugLogger('Listening on ' + bind);
 }
