3c1b6292cc4eb42b36d45b9404fe4a4ed8c1c4cc
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-07-25 15:09:32 -0700

    .

--- src/resources/scripts/mods/resourcepacks.ts
+++ src/resources/scripts/mods/resourcepacks.ts
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-	fetch("/resources/data/mods.json")
+document.addEventListener('DOMContentLoaded', function () {
+	fetch('/resources/data/mods.json')
 		.then((response) => response.json())
 		.then((data: { resourcepacks: { [x: string]: string; icon: string; author: string; description: string }[] }) => {
-			const modListElement = document.querySelector(".mod-list");
+			const modListElement = document.querySelector('.mod-list');
 			data.resourcepacks.forEach((mod: { [x: string]: string; icon: string; author: string; description: string }) => {
-				const modItem = document.createElement("div");
-				modItem.classList.add("mod-item");
+				const modItem = document.createElement('div');
+				modItem.classList.add('mod-item');
 				modItem.innerHTML = `
           <div class="mod-icon">
             <img src="${mod.icon}" />
           </div>
           <div class="mod-details">
-            <h3 class="mod-name">${mod["display-name"] ?? ""}</h3>
-            <p class="mod-author">By <a href="${mod["author-link"] ?? ""}" target="_blank">${mod.author}</a></p>
+            <h3 class="mod-name">${mod['display-name'] ?? ''}</h3>
+            <p class="mod-author">By <a href="${mod['author-link'] ?? ''}" target="_blank">${mod.author}</a></p>
             <p class="mod-description">${mod.description}</p>
             <div class="mod-links">
-              <a href="${mod["repo-link"] ?? ""}" class="mod-link" target="_blank">Repository</a>
-              <a href="${mod["download-link"] ?? ""}" class="mod-link" download>Download</a>
+              <a href="${mod['repo-link'] ?? ''}" class="mod-link" target="_blank">Repository</a>
+              <a href="${mod['download-link'] ?? ''}" class="mod-link" download>Download</a>
             </div>
           </div>
         `;
@@ -24,6 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
 			});
 		})
 		.catch((error: unknown) => {
-			console.error("Error fetching resource packs:", error);
+			console.error('Error fetching resource packs:', error);
 		});
 });
