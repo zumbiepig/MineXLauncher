a98f7b25f643f2e873bea47e17707e1da0a5cc3b
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2024-07-31 19:30:21 -0400

    yessir

---
+++ public/sw-register.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'my-app-cache-v1';
+const OFFLINE_URL = '/offline.html';
+
+// List of assets to cache
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    OFFLINE_URL,
+    '/styles.css', 
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request).catch(() => {
+                return caches.match(OFFLINE_URL);
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
