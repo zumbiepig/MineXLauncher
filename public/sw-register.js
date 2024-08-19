adcb2d5bddfa73e0c88788def611b4e030257286
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-08-19 14:07:21 -0700

    .

--- public/sw-register.js
+++ public/sw-register.js
@@ -6,10 +6,10 @@ if ('serviceWorker' in navigator) {
 				console.log('Service worker registered with scope:', registration.scope);
 
 				navigator.serviceWorker.addEventListener('message', (event) => {
-					if (event.data && event.data.type === 'install-progress') {
-						console.log(`Service worker installation progress: ${event.data.msg} assets`);
+					if (event.data.title === 'sw-install-progress') {
+						console.log(`Service worker installation progress: ${event.data.message} assets downloaded`);
 						// Update your UI with the progress
-					} else if (event.data && event.data.type === 'activation-complete') {
+					} else if (event.data.title === 'sw-install-complete') {
 						console.log('Service worker installation is complete!');
 						alert('Service worker installation is complete!');
 					}
