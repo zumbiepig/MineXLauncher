5a07d06cd22d58806ca46a2b3c9c40ef7a836a68
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-08-19 10:42:23 -0700

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,10 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
 	rules: {
 		'@typescript-eslint/ban-ts-comment': [
+			'error',
 			{
 				'ts-expect-error': false,
-        'ts-nocheck': false,
+				'ts-nocheck': false,
 				'ts-check': true,
 			},
 		],
