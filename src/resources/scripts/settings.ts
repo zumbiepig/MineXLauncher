8673dce8a0ee79a16b4189f079097327016b1881
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-08-18 20:31:12 -0700

    test offline and pwa

--- src/resources/scripts/settings.ts
+++ src/resources/scripts/settings.ts
@@ -1,5 +1,5 @@
 if (window.location.pathname === '/settings/') {
-	document.addEventListener('DOMContentLoaded', function () {
+	document.addEventListener('DOMContentLoaded', () => {
 		const profileName = document.getElementById('profile-name');
 		const usernameInput = document.getElementById('username-input') as HTMLInputElement;
 		const themeSelect = document.getElementById('theme-select') as HTMLSelectElement;
@@ -7,7 +7,7 @@ if (window.location.pathname === '/settings/') {
 		usernameInput.placeholder = storage.local.get('username') ?? '';
 		themeSelect.value = storage.local.get('theme') ?? '';
 
-		usernameInput.addEventListener('input', function () {
+		usernameInput.addEventListener('input', () => {
 			let username = usernameInput.value.replace(/[^A-Za-z0-9]/g, '_').substring(0, 16);
 			usernameInput.value = username;
 			while (username.length < 3) {
@@ -19,28 +19,28 @@ if (window.location.pathname === '/settings/') {
 			}
 		});
 
-		themeSelect.addEventListener('change', function () {
+		themeSelect.addEventListener('change', () => {
 			theme.set(themeSelect.value);
 		});
 	});
 }
 
 if (window.location.pathname === '/welcome.html') {
-	document.addEventListener('DOMContentLoaded', function () {
+	document.addEventListener('DOMContentLoaded', () => {
 		const setupForm = document.getElementById('setup-form') as HTMLFormElement;
 		const usernameInput = document.getElementById('username-input') as HTMLInputElement;
 		const themeSelect = document.getElementById('theme-select') as HTMLSelectElement;
 
-		usernameInput.addEventListener('input', function () {
+		usernameInput.addEventListener('input', () => {
 			const username = usernameInput.value.replace(/[^A-Za-z0-9]/g, '_').substring(0, 16);
 			usernameInput.value = username;
 		});
 
-		themeSelect.addEventListener('change', function () {
+		themeSelect.addEventListener('change', () => {
 			theme.load(themeSelect.value);
 		});
 
-		setupForm.addEventListener('submit', function (event) {
+		setupForm.addEventListener('submit', (event) => {
 			event.preventDefault();
 
 			let username = usernameInput.value.replace(/[^A-Za-z0-9]/g, '_').substring(0, 16);
@@ -56,6 +56,12 @@ if (window.location.pathname === '/welcome.html') {
 				storage.local.set('username', username);
 				storage.local.set('theme', themeSelect.value);
 				storage.local.set('lastVersion', launcherVersion);
+				try {
+					// @ts-expect-error 123
+					window.top?.installPwaEvent.prompt();
+				} catch (error) {
+					console.warn('Failed to prompt PWA install:', error);
+				}
 				// @ts-expect-error 123
 				window.top.location.href = '/';
 			}
