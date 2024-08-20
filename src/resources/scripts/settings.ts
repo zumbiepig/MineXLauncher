eb738e6bd860d79d6cd842b605bfc36d225ff85e
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-08-19 17:14:36 -0700

    .

--- src/resources/scripts/settings.ts
+++ src/resources/scripts/settings.ts
@@ -3,6 +3,7 @@ if (window.location.pathname === '/settings/') {
 		const profileName = document.getElementById('profile-name');
 		const usernameInput = document.getElementById('username-input') as HTMLInputElement;
 		const themeSelect = document.getElementById('theme-select') as HTMLSelectElement;
+		const offlineCheckbox = document.getElementById('offline-checkbox') as HTMLInputElement;
 
 		usernameInput.placeholder = storage.local.get('username') ?? '';
 		themeSelect.value = storage.local.get('theme') ?? '';
@@ -22,6 +23,17 @@ if (window.location.pathname === '/settings/') {
 		themeSelect.addEventListener('change', () => {
 			theme.set(themeSelect.value);
 		});
+
+		offlineCheckbox.addEventListener('change', () => {
+			storage.local.set('offlineCache', offlineCheckbox.checked);
+			if (offlineCheckbox.checked) {
+				serviceworker.register();
+				alert('Offline cache is now downloading.\nThe download size is about 1GB, so it may take a while.');
+			} else {
+				serviceworker.unregister();
+				alert('Offline cache has been deleted.');
+			}
+		});
 	});
 }
 
@@ -30,6 +42,7 @@ if (window.location.pathname === '/welcome.html') {
 		const setupForm = document.getElementById('setup-form') as HTMLFormElement;
 		const usernameInput = document.getElementById('username-input') as HTMLInputElement;
 		const themeSelect = document.getElementById('theme-select') as HTMLSelectElement;
+		const offlineCheckbox = document.getElementById('offline-checkbox') as HTMLInputElement;
 
 		usernameInput.addEventListener('input', () => {
 			const username = usernameInput.value.replace(/[^A-Za-z0-9]/g, '_').substring(0, 16);
@@ -56,13 +69,18 @@ if (window.location.pathname === '/welcome.html') {
 
 				storage.local.set('username', username);
 				storage.local.set('theme', themeSelect.value);
+				storage.local.set('offlineCache', offlineCheckbox.checked);
 				storage.local.set('lastVersion', launcherVersion);
 
-				try {
-					// @ts-expect-error
-					installPwaEvent.prompt();
-				} catch (error) {
-					console.log('Failed to prompt PWA install:', error);
+				if (offlineCheckbox.checked) {
+					serviceworker.register();
+					alert('Offline cache is now downloading.\nThe download size is about 1GB, so it may take a while.');
+					try {
+						// @ts-expect-error
+						installPwaEvent.prompt();
+					} catch (error) {
+						console.log('Failed to prompt PWA install:', error);
+					}
 				}
 
 				// @ts-expect-error
