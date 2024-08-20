73d15bb189fd3e8d82f8c7a4ed3815dc4e05f2e4
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-08-19 17:43:55 -0700

    fix

--- src/resources/scripts/main.ts
+++ src/resources/scripts/main.ts
@@ -293,11 +293,19 @@ const serviceworker = {
 		if ('serviceWorker' in navigator) {
 			navigator.serviceWorker.getRegistrations().then((registrations) => {
 				for (const registration of registrations) {
-					registration.unregister();
+					registration.unregister().then(() => {
+						caches.keys().then((keyList) => {
+							return Promise.all(
+								keyList.map((key) => {
+									return caches.delete(key);
+								})
+							);
+						});
+					});
 				}
 			});
 		}
-	}
+	},
 };
 
 if (window.location.pathname === '/') {
