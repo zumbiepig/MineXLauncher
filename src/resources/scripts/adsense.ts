976b37fed3a058c2ac6289713547f0da6bdece3f
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-08-21 15:07:38 -0700

    .

---
+++ src/resources/scripts/adsense.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+const script = document.createElement('script');
+script.async = true;
+script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1132419379737567';
+script.crossOrigin = 'anonymous';
+document.head.appendChild(script);
