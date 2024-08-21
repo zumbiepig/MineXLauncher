6f69f973b4ff79022280eb46f6835cce2c81fd47
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-08-21 16:42:30 -0700

    remove that

--- src/resources/scripts/main.ts
+++ src/resources/scripts/main.ts
@@ -343,8 +343,7 @@ if (window.location.pathname === '/') {
 			alert(`MineXLauncher has been updated to v${launcherVersion}!
 
 Changes in v${launcherVersion}:
-  - You can now install the launcher as a PWA web app
-  - Ads have been added, join the Discord server to disable them`);
+  - You can now install the launcher as a PWA web app`);
 			storage.local.set('lastVersion', launcherVersion);
 		}
 	});
