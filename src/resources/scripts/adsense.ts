e39346a2ebacb167a1eced4a99ad6fd308ba6b49
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-08-21 15:59:33 -0700

    .

--- src/resources/scripts/adsense.ts
+++
@@ -1,5 +0,0 @@
-const adsense = document.createElement('script');
-adsense.async = true;
-adsense.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1132419379737567';
-adsense.crossOrigin = 'anonymous';
-document.head.appendChild(adsense);
--- src/resources/scripts/main.ts
+++ src/resources/scripts/main.ts
@@ -322,6 +322,14 @@ if (window.location.pathname === '/') {
 	} */
 	serviceworker.register('/sw.js');
 } else {
+	if (storage.local.get('showAds') !== false) {
+		const adsense = document.createElement('script');
+		adsense.async = true;
+		adsense.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1132419379737567';
+		adsense.crossOrigin = 'anonymous';
+		document.head.appendChild(adsense);
+	}
+
 	document.addEventListener('DOMContentLoaded', () => {
 		const profileName = document.getElementById('profile-name');
 		if (profileName) {
