4747d8955b45e64d2999352f8217949eda28c380
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-08-29 09:02:54 -0700

    update launch

--- src/resources/scripts/eagler-launch/1.5.2/main.ts
+++ src/resources/scripts/eagler-launch/1.5.2/main.ts
@@ -10,18 +10,31 @@ window.addEventListener('load', function () {
 			{ serverName: 'BrickMC', serverAddress: 'wss://play.brickmc.net', hideAddress: false },
 		],
 		relays: [
-			{ addr: 'wss://relay.deev.is/', name: 'lax1dude relay #1', primary: relayId == 0 },
-			{ addr: 'wss://relay.lax1dude.net/', name: 'lax1dude relay #2', primary: relayId == 1 },
-			{ addr: 'wss://relay.shhnowisnottheti.me/', name: 'ayunami relay #1', primary: relayId == 2 },
+			{ addr: 'wss://relay.deev.is/', name: 'lax1dude relay #1', primary: relayId === 0 },
+			{ addr: 'wss://relay.lax1dude.net/', name: 'lax1dude relay #2', primary: relayId === 1 },
+			{ addr: 'wss://relay.shhnowisnottheti.me/', name: 'ayunami relay #1', primary: relayId === 2 },
 		],
-		mainMenu: { splashes: ['Darviglet!', 'eaglerenophile!', 'You Eagler!', 'Yeeeeeee!', 'yeee', 'EEEEEEEEE!', 'You Darvig!', 'You Vigg!', ':>', '|>', 'You Yumpster!'], eaglerLogo: false },
+		mainMenu: {
+			splashes: [
+				'Darviglet!',
+				'eaglerenophile!',
+				'You Eagler!',
+				'Yeeeeeee!',
+				'yeee',
+				'EEEEEEEEE!',
+				'You Darvig!',
+				'You Vigg!',
+				':>',
+				'|>',
+				'You Yumpster!'
+			],
+			eaglerLogo: false
+		},
 	};
 
 	const urlParams = new URLSearchParams(window.location.search);
 	const server = urlParams.get('server');
-	if (server) {
-		window.eaglercraftOpts.joinServer = server;
-	}
+	if (server) window.eaglercraftOpts.joinServer = server;
 
 	main();
 });
