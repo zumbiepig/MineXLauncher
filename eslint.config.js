12958cdb38a61677e6e003b7f3e4836b448df328
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-09-07 08:30:51 -0700

    .

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
 	eslint.configs.recommended,
 	tseslint.configs.eslintRecommended,
 	...tseslint.configs.strict,
 	...tseslint.configs.stylisticTypeChecked,
+	eslintConfigPrettier,
 	{
 		languageOptions: {
 			parserOptions: {
