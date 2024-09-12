d554212037f352abd8b7c1bd39fd90cfddd4cd6f
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-09-12 15:06:20 -0700

    .

--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
-// @ts-nocheck
 import { join } from 'path';
-import { env } from 'bun';
-import express, { json, urlencoded } from 'express';
 import chalk from 'chalk';
+
+import express, {
+	json,
+	urlencoded,
+	type Request,
+	type Response,
+	type NextFunction,
+} from 'express';
+
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
-import debug from 'debug';
-import errorhandler from 'errorhandler';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import serveFavicon from 'serve-favicon';
@@ -15,15 +19,12 @@ import serveStatic from 'serve-static';
 import { indexRouter } from './routes/index.ts';
 
 const BASE_DIR = join(import.meta.dir, 'public');
-const PORT = env.PORT ?? 3000;
+const PORT = process.env['PORT'] ?? 3000;
 
-const debugLogger = debug('minexlauncher:server');
-const isDev = env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'development';
 
 const app = express();
 app.enable('strict routing');
-
-debugLogger('Booting server.');
 
 app.use(
 	helmet({
@@ -38,8 +39,7 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(compression());
-
-if (isDev) app.use(errorhandler());
+if (isDev) app.use((await import('errorhandler')).default());
 
 app.use(
 	serveFavicon(join(BASE_DIR, 'resources/images/icons/favicon.webp'), {
@@ -50,23 +50,22 @@ app.use(serveStatic(BASE_DIR));
 
 app.use('/', indexRouter);
 
-app.use(async (req, res, next) => {
+app.use((_req: Request, res: Response, next: NextFunction) => {
 	res.status(404).sendFile(join(BASE_DIR, '404.html'));
 	next();
 });
 
-app.use(async (err, req, res, next) => {
+app.use((err: Error, _req: Request, res: Response, next: NextFunction) => {
 	console.error(err.stack);
 	res.status(500).send('500 Internal Server Error');
 	next();
 });
-// push for the update dev
+
 app
-	.listen(PORT, async () => {
-		debugLogger('Server started.');
-		console.log(chalk.green(`Server is running on port ${PORT}`));
-	})
-	.on('error', (error) => {
+	.listen(PORT, () =>
+		console.log(chalk.green(`Server is running on port ${PORT}`)),
+	)
+	.on('error', (error: NodeJS.ErrnoException) => {
 		if (error.code === 'EADDRINUSE') {
 			console.error(
 				chalk.red('EADDRINUSE') +
