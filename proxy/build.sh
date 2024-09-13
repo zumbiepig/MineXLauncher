305a3bdf7c5bd45effbfcd7d87e0a106acdf0f5b
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2024-09-13 15:36:01 -0700

    .

--- proxy/build.sh
+++ proxy/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'Make sure you are running this in the `proxy` directory' &&
-rm -r ./bin &&
+rm -rf ./bin &&
 mkdir ./bin &&
 bun build ./proxy.ts --compile --minify --target=bun-linux-x64-modern --outfile ./bin/linux-x64 &&
 chmod +x ./bin/linux-x64 &&
